08668e7bb84279791657a547bffa816096b053f5
diloes <diloes@hotmail.com>
2021-04-28 18:49:18 +0200

    Add files via upload

--- 02 - faq-accordion-card-main/js/main.js
+++ 02 - faq-accordion-card-main/js/main.js
@@ -6,8 +6,24 @@ let preguntaUna;
 
 function iniciarApp(){
 
-    //Seleccionamos las preguntas
     const pregunta = document.querySelectorAll('.colocar-flecha');
-    pregunta.forEach(element => {
-        preguntaUna = element;
+    const respuesta = document.querySelectorAll('.respuesta');
+    
+    for(let i=0; i < pregunta.length; i++){
+        //console.log(respuesta[i].classList.value)
+        pregunta[i].addEventListener('click', function(){
+
+            respuesta[i].classList.remove('ocultar');
+
+            // if(respuesta[i].classList.value !== 'respuesta ocultar'){
+            //     respuesta[i].classList.add('ocultar');
+            // }
+
+        });
+    }
+
+    
 }
+
+
+
